d60c38225bd92ccef142399720847b9e6aaf4e48
aysenurbolat <bolataysenur95@gmail.com>
2020-09-11 14:08:14 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "include/inverted_index.hpp"
+
+int main(int argc, char* argv[]) 
+{
+  
+    InvertIndex a;
+    
+
+    a.addFile("C:\\Users\\Lenovo\\Desktop\\library");
+
+    a.showFiles();
+ 
+ 
+}
