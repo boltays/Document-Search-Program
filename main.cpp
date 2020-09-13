84e3a9516fdb5c979f78d5a9700232b6411ef545
aysenurbolat <bolataysenur95@gmail.com>
2020-09-13 14:28:54 +0300

    Added cmd utility function call

--- main.cpp
+++ main.cpp
@@ -6,10 +6,11 @@ int main(int argc, char* argv[])
     InvertIndex a;
     
 
-    a.addFile("C:\\Users\\Lenovo\\Desktop\\library");
-    a.showFiles();
-	a.indexAllDir();
-	a.showAllIndex();
- 
+    //a.addFile("C:\\Users\\Lenovo\\Desktop\\library");
+    //a.showFiles();
+	//a.indexAllDir();
+	//a.showAllIndex();
+	commandLineUtility(a, argc, argv);
+	
  
 }
