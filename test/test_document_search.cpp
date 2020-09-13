43209f486f03a47642f3424ea54cb4a342d4c724
aysenurbolat <bolataysenur95@gmail.com>
2020-09-13 19:04:52 +0300

    Tested parser and show funcs

--- test/test_document_search.cpp
+++ test/test_document_search.cpp
@@ -34,6 +34,16 @@ void determineExactPathLibrary(string & pathOfLibrary)
 	pathOfLibrary = exactPath + slashForPath + "library";
 }
 
+int compareSizeOfContainers(int size1, int size2)
+{
+	return (size1 == size2);
+}
+
+int compareTwoMap(map<string, wordOccurences> map1, map<string, wordOccurences> map2)
+{
+    return (map1 == map2);    
+}
+
 TEST_F(InIndex, iterate_through_given_library)
 {
 	InvertedIndex obj1;
@@ -46,4 +56,39 @@ TEST_F(InIndex, iterate_through_given_library)
 	obj2.addFile(pathOfLibrary);
 	
 	ASSERT_EQ(compareTwoVector(obj1.getFileList(), obj2.getFileList()), 1);	
+}
+
+TEST_F(InIndex, test_show_files)
+{
+	InvertedIndex obj1;
+	InvertedIndex obj2;
+	
+	string pathOfLibrary;
+    determineExactPathLibrary(pathOfLibrary);
+    
+    obj1.addFile(pathOfLibrary);
+    
+    stringstream ss;
+    ASSERT_EQ(obj1.showFiles(ss), 0);
+    ASSERT_EQ(obj2.showFiles(ss), 1);	
+}
+
+TEST_F(InIndex, test_parser_indexer)
+{
+	InvertedIndex obj1;
+	InvertedIndex obj2;
+	InvertedIndex obj3;
+	
+	string pathOfLibrary;
+	determineExactPathLibrary(pathOfLibrary);
+    obj1.addFile(pathOfLibrary);
+    obj2.addFile(pathOfLibrary);
+    
+	obj1.indexAllDir();
+	obj2.indexAllDir();
+	
+	ASSERT_EQ(compareTwoMap(obj1.getOccurenceInFile(), obj3.getOccurenceInFile()), 0);
+	ASSERT_EQ(compareTwoMap(obj1.getOccurenceInFile(), obj2.getOccurenceInFile()), 1);
+	ASSERT_EQ(compareSizeOfContainers(obj1.getOccurenceInFile().size(), obj2.getOccurenceInFile().size()), 1);
+	ASSERT_EQ(compareSizeOfContainers(obj1.getOccurenceInFile().size(), obj3.getOccurenceInFile().size()), 0);
 }
