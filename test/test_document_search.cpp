7b88788fccfaafa0468fc422154ba0922696621c
aysenurbolat <bolataysenur95@gmail.com>
2020-09-13 17:51:41 +0300

    Created test source file

---
+++ test/test_document_search.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+#include <climits>
+#include "../include/inverted_index.hpp"
+
+
+class InIndex : public testing::Test
+{
+public:
+    void SetUp() override
+    {
+    }
+
+    void TearDown() override
+    {
+    }
+};
+
+
+TEST_F(InIndex, iterate_through_given_library)
+{
+	
+}
