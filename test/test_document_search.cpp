4cf411aa3f326ab7007e780c7c03d4b5d376a92f
aysenurbolat <bolataysenur95@gmail.com>
2020-09-13 18:29:45 +0300

    Tested addFile function

--- test/test_document_search.cpp
+++ test/test_document_search.cpp
@@ -3,6 +3,8 @@
 #include "../include/inverted_index.hpp"
 
 
+extern string slashForPath;
+
 class InIndex : public testing::Test
 {
 public:
@@ -16,7 +18,32 @@ public:
 };
 
 
+int compareTwoVector(vector<string> vec1, vector<string> vec2)
+{
+	return (vec1 == vec2);
+}
+
+void determineExactPathLibrary(string & pathOfLibrary)
+{
+	string a  = std::filesystem::current_path().u8string();
+	string stem = fs::current_path().stem().u8string();
+	
+	string exactPath = a.erase(a.find(stem));
+	exactPath = exactPath.erase(exactPath.find(slashForPath + "build"));
+	
+	pathOfLibrary = exactPath + slashForPath + "library";
+}
+
 TEST_F(InIndex, iterate_through_given_library)
 {
+	InvertedIndex obj1;
+	InvertedIndex obj2;
 	
+	string pathOfLibrary;
+	determineExactPathLibrary(pathOfLibrary);
+	
+	obj1.addFile(pathOfLibrary);
+	obj2.addFile(pathOfLibrary);
+	
+	ASSERT_EQ(compareTwoVector(obj1.getFileList(), obj2.getFileList()), 1);	
 }
